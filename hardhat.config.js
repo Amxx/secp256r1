e796a1f9f90a7045167437a227badddc6ed09fbf
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-01-19 16:40:03 +0100

    wip

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,11 @@ require('@nomicfoundation/hardhat-ethers');
 module.exports = {
   solidity: {
     compilers: [
-      { version: '0.8.23' },
-      { version: '0.7.6' },
-      { version: '0.6.12' },
-      { version: '0.5.17' },
-      { version: '0.4.26' },
+      { version: '0.8.23', settings: { optimizer: { enabled: true, runs: 200 }} },
+      { version: '0.7.6',  settings: { optimizer: { enabled: true, runs: 200 }} },
+      { version: '0.6.12', settings: { optimizer: { enabled: true, runs: 200 }} },
+      { version: '0.5.17', settings: { optimizer: { enabled: true, runs: 200 }} },
+      { version: '0.4.26', settings: { optimizer: { enabled: true, runs: 200 }} },
     ],
   },
 };

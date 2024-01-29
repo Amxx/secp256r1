5e5fde397c9649e6fe3bc688841ff227576774d1
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-01-29 15:39:05 +0100

    new version

--- contracts/P256.sol
+++ contracts/P256.sol
@@ -1,25 +1,27 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
 
 import { Math } from "./Math.sol";
 
-/**
- * @dev Implementation of secp256r1 verification.
- * Inspired by https://github.com/maxrobot/elliptic-solidity/blob/master/contracts/Secp256r1.sol
- * and optimized for solidity ^0.8.0.
- */
+// Heavily inspired from
+// https://github.com/maxrobot/elliptic-solidity/blob/master/contracts/Secp256r1.sol
+// https://github.com/tdrerup/elliptic-curve-solidity/blob/master/contracts/curves/EllipticCurve.sol
+// and modified jacobian double
+// optimisations to avoid to an from from affine and jacobian coordinates
 library P256 {
+    struct JPoint {
+        uint256 x;
+        uint256 y;
+        uint256 z;
+    }
+
     uint256 constant gx = 0x6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296;
     uint256 constant gy = 0x4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5;
     uint256 constant pp = 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF;
     uint256 constant nn = 0xFFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551;
-    uint256 constant a  = 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC;
-    uint256 constant b  = 0x5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B;
-    uint256 constant ss = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0;
-
-    function getPublicKey(uint256 privateKey) internal pure returns (uint256, uint256) {
-        return scalarMult(gx, gy, privateKey);
-    }
+    uint256 constant a = 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC;
+    uint256 constant b = 0x5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B;
+    uint256 constant MOST_SIGNIFICANT = 0xc000000000000000000000000000000000000000000000000000000000000000;
 
     /**
      * @dev signature verification
@@ -27,21 +29,17 @@ library P256 {
      * @param py - public key coordinate Y
      * @param r - signature half R
      * @param s - signature half S
-     * @param digest - hashed message
+     * @param e - hashed message
      */
-    function verify(uint256 px, uint256 py, uint256 r, uint256 s, bytes32 digest) internal pure returns (bool) {
-        // Should be s >= ss, but tolling generates signatures with points on both sides.
+    function verify(uint256 px, uint256 py, uint256 r, uint256 s, uint256 e) internal view returns (bool) {
         if (r >= nn || s >= nn) return false;
 
-        uint256 d = uint256(digest) % nn;
-        uint256 w = Math.invMod(s, nn);
-        uint256 u1 = mulmod(d, w, nn);
+        JPoint[16] memory points = _preComputeJacobianPoints(px, py);
+        uint256 w = _primemod(s, nn);
+        uint256 u1 = mulmod(e, w, nn);
         uint256 u2 = mulmod(r, w, nn);
-        (uint256 x1, uint256 y1) = scalarMult(gx, gy, u1);
-        (uint256 x2, uint256 y2) = scalarMult(px, py, u2);
-        (uint256 x, ) = add(x1, y1, x2, y2);
-
-        return (x % pp == r);
+        (uint256 x, ) = _shamirMultJacobian(points, u1, u2);
+        return (x == r);
     }
 
     /**
@@ -49,10 +47,9 @@ library P256 {
      * @param r - signature half R
      * @param s - signature half S
      * @param v - signature recovery param
-     * @param digest - hashed message
+     * @param e - hashed message
      */
-    function recovery(uint256 r, uint256 s, uint8 v, bytes32 digest) internal view returns (uint256, uint256) {
-        // Should be s >= ss, but tolling generates signatures with points on both sides.
+    function recovery(uint256 r, uint256 s, uint8 v, uint256 e) internal view returns (uint256, uint256) {
         if (r >= nn || s >= nn || v > 1) return (0, 0);
 
         uint256 rx = r;
@@ -61,59 +58,106 @@ library P256 {
         if (mulmod(ry, ry, pp) != ry2) return (0, 0);
         if (ry % 2 != v % 2) ry = pp - ry;
 
-        uint256 d = uint256(digest) % nn;
-        uint256 w = Math.invMod(r, nn);
-        uint256 u1 = mulmod(nn - d, w, nn);
+        JPoint[16] memory points = _preComputeJacobianPoints(rx, ry);
+        uint256 w = _primemod(r, nn);
+        uint256 u1 = mulmod(nn - (e % nn), w, nn);
         uint256 u2 = mulmod(s, w, nn);
-        (uint256 x1, uint256 y1) = scalarMult(gx, gy, u1);
-        (uint256 x2, uint256 y2) = scalarMult(rx, ry, u2);
-        return add(x1, y1, x2, y2);
+        (uint256 x, uint256 y) = _shamirMultJacobian(points, u1, u2);
+        return (x, y);
     }
 
-    function add(uint256 p1, uint256 p2, uint256 q1, uint256 q2) internal pure returns(uint256, uint256) {
-        uint256 p3;
-        (p1, p2, p3) = _jAdd(p1, p2, uint256(1), q1, q2, uint256(1));
-        return _affineFromJacobian(p1, p2, p3);
-    }
-
-    function double(uint256 p1, uint256 p2) internal pure returns(uint256, uint256) {
-        uint256 p3;
-        (p1, p2, p3) = _jDouble(p1, p2, uint256(1));
-        return _affineFromJacobian(p1, p2, p3);
-    }
-
-    function scalarMult(uint256 Bx, uint256 By, uint256 k) internal pure returns (uint256, uint256) {
+    function getPublicKey(uint256 privateKey) internal view returns (uint256, uint256) {
+        uint256 k = privateKey;
         uint256 x = 0;
         uint256 y = 0;
         uint256 z = 0;
-
-        for (uint256 i = 0; i < 256; ++i) {
-            (x, y, z) = _jDouble(x, y, z);
-            if ((k >> (255 - i)) & 0x1 == 0x1) {
-                (x, y, z) = _jAdd(Bx, By, 1, x, y, z);
+        unchecked {
+            for (uint256 i = 0; i < 256; ++i) {
+                (x, y, z) = _jDouble(x, y, z);
+                if ((k >> (255 - i)) & 0x1 == 0x1) {
+                    (x, y, z) = _jAdd(gx, gy, 1, x, y, z);
+                }
             }
         }
-
         return _affineFromJacobian(x, y, z);
     }
 
     /**
-     * @dev translate jacobian to affine coordinates
+     * Strauss Shamir trick for EC multiplication
+     * https://stackoverflow.com/questions/50993471/ec-scalar-multiplication-with-strauss-shamir-method
+     * we optimise on this a bit to do with 2 bits at a time rather than a single bit
+     * the individual points for a single pass are precomputed
+     * overall this reduces the number of additions while keeping the same number of doublings
      */
-    function _affineFromJacobian(uint256 x, uint256 y, uint256 z) private pure returns(uint256 ax, uint256 ay) {
+    function _shamirMultJacobian(JPoint[16] memory points, uint256 u1, uint256 u2) private view returns (uint256, uint256) {
+        uint256 x = 0;
+        uint256 y = 0;
+        uint256 z = 0;
+        uint256 i = 0;
+        uint256 bits = 128;
+        unchecked {
+            while (bits > 0) {
+                if (z > 0) {
+                    (x, y, z) = _jDouble(x, y, z);
+                    (x, y, z) = _jDouble(x, y, z);
+                }
+                i = ((u1 & MOST_SIGNIFICANT) >> 252) | ((u2 & MOST_SIGNIFICANT) >> 254);
+                if (i > 0) {
+                    (x, y, z) = _jAdd(x, y, z, points[i].x, points[i].y, points[i].z);
+                }
+                u1 <<= 2;
+                u2 <<= 2;
+                --bits;
+            }
+        }
+        return _affineFromJacobian(x, y, z);
+    }
+
+    function _preComputeJacobianPoints(uint256 px, uint256 py) private pure returns (JPoint[16] memory points) {
+        points[0x00] = JPoint(0, 0, 0);
+        points[0x01] = JPoint(px, py, 1);
+        points[0x04] = JPoint(gx, gy, 1);
+        points[0x02] = _jPointDouble(points[0x01]);
+        points[0x08] = _jPointDouble(points[0x04]);
+        points[0x03] = _jPointAdd(points[0x01], points[0x02]);
+        points[0x05] = _jPointAdd(points[0x01], points[0x04]);
+        points[0x06] = _jPointAdd(points[0x02], points[0x04]);
+        points[0x07] = _jPointAdd(points[0x03], points[0x04]);
+        points[0x09] = _jPointAdd(points[0x01], points[0x08]);
+        points[0x0a] = _jPointAdd(points[0x02], points[0x08]);
+        points[0x0b] = _jPointAdd(points[0x03], points[0x08]);
+        points[0x0c] = _jPointAdd(points[0x04], points[0x08]);
+        points[0x0d] = _jPointAdd(points[0x01], points[0x0c]);
+        points[0x0e] = _jPointAdd(points[0x02], points[0x0c]);
+        points[0x0f] = _jPointAdd(points[0x03], points[0x0C]);
+    }
+
+    function _jPointAdd(JPoint memory p1, JPoint memory p2) private pure returns (JPoint memory) {
+        (uint256 x, uint256 y, uint256 z) = _jAdd(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z);
+        return JPoint(x, y, z);
+    }
+
+    function _jPointDouble(JPoint memory p) private pure returns (JPoint memory) {
+        (uint256 x, uint256 y, uint256 z) = _jDouble(p.x, p.y, p.z);
+        return JPoint(x, y, z);
+    }
+
+    /**
+     * @dev returns affine coordinates from a jacobian input follows golang elliptic/crypto library
+     */
+    function _affineFromJacobian(uint256 x, uint256 y, uint256 z) private view returns (uint256 ax, uint256 ay) {
         if (z == 0) return (0, 0);
-        uint256 zinv = Math.invMod(z, pp);
+        uint256 zinv = _primemod(z, pp);
         uint256 zinvsq = mulmod(zinv, zinv, pp);
         uint256 zinvcb = mulmod(zinvsq, zinv, pp);
         ax = mulmod(x, zinvsq, pp);
         ay = mulmod(y, zinvcb, pp);
     }
-
     /**
-     * @dev add two Jacobian points as described in:
+     * @dev performs double Jacobian as defined below:
      * https://hyperelliptic.org/EFD/g1p/auto-code/shortw/jacobian-3/doubling/mdbl-2007-bl.op3
      */
-    function _jAdd(uint256 p1, uint256 p2, uint256 p3, uint256 q1, uint256 q2, uint256 q3) private pure returns(uint256 r1, uint256 r2, uint256 r3) {
+    function _jAdd(uint256 p1, uint256 p2, uint256 p3, uint256 q1, uint256 q2, uint256 q3) private pure returns (uint256 r1, uint256 r2, uint256 r3) {
         if (p3 == 0) {
             return (q1, q2, q3);
         }
@@ -121,7 +165,7 @@ library P256 {
             return (p1, p2, p3);
         }
         assembly {
-            let pd := pp
+            let pd := 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF
             let z1z1 := mulmod(p3, p3, pd) // Z1Z1 = Z1^2
             let z2z2 := mulmod(q3, q3, pd) // Z2Z2 = Z2^2
 
@@ -131,100 +175,72 @@ library P256 {
             let s1 := mulmod(p2, mulmod(z2z2, q3, pd), pd) // S1 = Y1*Z2*Z2Z2
             let s2 := mulmod(q2, mulmod(z1z1, p3, pd), pd) // S2 = Y2*Z1*Z1Z1
 
-            mstore(0x02A0, addmod(p3, q3, pd))
+            let p3q3 := addmod(p3, q3, pd)
 
-            if lt(u2, u1) {
-                u2 := add(pd, u2) // u2 = u2+pd
-            }
+            if lt(u2, u1) { u2 := add(pd, u2) } // u2 = u2+pd
+
             let h := sub(u2, u1) // H = U2-U1
 
-            let i := mulmod(mulmod(0x02, h, pd), mulmod(0x02, h, pd), pd) // I = (2*H)^2
+            let i := mulmod(0x02, h, pd)
+            i := mulmod(i, i, pd) // I = (2*H)^2
 
             let j := mulmod(h, i, pd) // J = H*I
-            if lt(s2, s1) {
-                s2 := add(pd, s2) // u2 = u2+pd
-            }
-            let rr := mulmod(0x02, sub(s2, s1), pd) // r = 2*(S2-S1)
+            if lt(s2, s1) { s2 := add(pd, s2) } // u2 = u2+pd
 
-            let v := mulmod(u1, i, pd) // V = U1*I
+            let rr := mulmod(0x02, sub(s2, s1), pd) // r = 2*(S2-S1)
             r1 := mulmod(rr, rr, pd) // X3 = R^2
 
-            mstore(0x0260, addmod(j, mulmod(0x02, v, pd), pd)) // I = J+(2*V)
-            if lt(r1, mload(0x0260)) {
-                r1 := add(pd, r1) // X3 = X3+pd
-            }
-            r1 := sub(r1, mload(0x0260))
+            let v := mulmod(u1, i, pd) // V = U1*I
+            let j2v := addmod(j, mulmod(0x02, v, pd), pd)
+            if lt(r1, j2v) { r1 := add(pd, r1) } // X3 = X3+pd
+
+            r1 := sub(r1, j2v)
 
             // Y3 = r*(V-X3)-2*S1*J
-            mstore(0x0220, mulmod(0x02, s1, pd))
-            mstore(0x0220, mulmod(mload(0x0220), j, pd))
+            let s12j := mulmod(mulmod(0x02, s1, pd), j, pd)
 
-            if lt(v, r1) {
-                v := add(pd, v)
-            }
-            mstore(0x0240, sub(v, r1))
-            mstore(0x0240, mulmod(rr, mload(0x240), pd))
+            if lt(v, r1) { v := add(pd, v) }
+            r2 := mulmod(rr, sub(v, r1), pd)
 
-            if lt(mload(0x0240), mload(0x0220)) {
-                mstore(0x0240, add(mload(0x0240), pd))
-            }
-            mstore(0x0240, sub(mload(0x0240), mload(0x0220)))
-            r2 := mload(0x0240)
+            if lt(r2, s12j) { r2 := add(pd, r2) }
+            r2 := sub(r2, s12j)
 
             // Z3 = ((Z1+Z2)^2-Z1Z1-Z2Z2)*H
             z1z1 := addmod(z1z1, z2z2, pd)
-            mstore(0x0260, mulmod(mload(0x02A0), mload(0x02A0), pd))
-            // r3 := mload(0x0260)
-            if lt(mload(0x0260), z1z1) {
-                mstore(0x0260, add(pd, mload(0x0260)))
-            }
-            r3 := mulmod(sub(mload(0x0260), z1z1), h, pd)
+            j2v := mulmod(p3q3, p3q3, pd)
+            if lt(j2v, z1z1) { j2v := add(pd, j2v) }
+            r3 := mulmod(sub(j2v, z1z1), h, pd)
         }
         return (r1, r2, r3);
     }
 
-    /**
-     * @dev double a Jacobian point as described in:
-     * https://hyperelliptic.org/EFD/g1p/auto-code/shortw/jacobian-3/doubling/dbl-2001-b.op3
-     */
-    function _jDouble(uint256 p1, uint256 p2, uint256 p3) private pure returns(uint256 q1, uint256 q2, uint256 q3) {
+    // Point doubling on the modified jacobian coordinates
+    // http://point-at-infinity.org/ecc/Prime_Curve_Modified_Jacobian_Coordinates.html
+    function _jDouble(uint256 x, uint256 y, uint256 z) private pure returns (uint256 x3, uint256 y3, uint256 z3) {
         assembly {
-            let pd := pp
-            let delta := mulmod(p3, p3, pd) // delta = Z1^2
-            let gamma := mulmod(p2, p2, pd) // gamma = Y1^2
-            let beta := mulmod(p1, gamma, pd) // beta = X1*gamma
-
-            let alpha := p1
-            if lt(alpha, delta) {
-                alpha := add(pd, alpha)
-            }
-            alpha := mulmod(0x03 , mulmod(sub(alpha, delta), addmod(p1, delta, pd), pd), pd) // alpha = 3*(X1-delta)*(X1+delta)
-
-            q1 := mulmod(alpha, alpha, pd)
-            if lt(q1, mulmod(0x08, beta, pd)) {
-                q1 := add(pd, q1)
-            }
-            q1 := sub(q1, mulmod(0x08, beta, pd)) // X3 = (alpha^2)-(8*beta)
-
-            q3  := addmod(p2, p3, pd)
-            q3 := mulmod(q3, q3, pd)
-
-            delta := addmod(delta, gamma, pd)
-            if lt(q3, delta) {
-                q3 := add(pd, q3)
-            }
-            q3 := sub(q3, delta) // Z3 = (Y1+Z1)^2-gamma-delta
-
-            q2 := mulmod(0x04, beta, pd)
-            if lt(q2, q1) {
-                q2 := add(pd, q2)
-            }
-            q2 := mulmod(alpha, sub(q2, q1), pd)
-            gamma := mulmod(0x08, mulmod(gamma, gamma, pd), pd)
-            if lt(q2, gamma) {
-                q2 := add(pd, q2)
-            }
-            q2 := sub(q2, gamma) // Y3 = alpha*(4*beta-X3)-8*gamma^2
+            let pd := 0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF
+            let z2 := mulmod(z, z, pd)
+            let az4 :=
+                mulmod(0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC, mulmod(z2, z2, pd), pd)
+            let y2 := mulmod(y, y, pd)
+            let s := mulmod(0x04, mulmod(x, y2, pd), pd)
+            let u := mulmod(0x08, mulmod(y2, y2, pd), pd)
+            let m := addmod(mulmod(0x03, mulmod(x, x, pd), pd), az4, pd)
+            let twos := mulmod(0x02, s, pd)
+            let m2 := mulmod(m, m, pd)
+            if lt(m2, twos) { m2 := add(pd, m2) }
+            x3 := sub(m2, twos)
+            if lt(s, x3) { s := add(pd, s) }
+            y3 := mulmod(m, sub(s, x3), pd)
+            if lt(y3, u) { y3 := add(pd, y3) }
+            y3 := sub(y3, u)
+            z3 := mulmod(0x02, mulmod(y, z, pd), pd)
         }
+    }
+
+    // From fermats little theorem https://en.wikipedia.org/wiki/Fermat%27s_little_theorem:
+    // `a**(p-1) ≡ 1 mod p`. This means that `a**(p-2)` is an inverse of a in Fp.
+    function _primemod(uint256 value, uint256 p) private view returns (uint256) {
+        return Math.modExp(value, p - 2, p);
     }
 }
---
+++ test/metrics.test.js
@@ -0,0 +1,79 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+const { secp256r1 } = require('@noble/curves/p256');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
+const RUN_COUNT = 10;
+
+describe('gas metrics', function () {
+  before(async function () {
+    this.metrics = {};
+  });
+
+  after(async function () {
+    Object.entries(this.metrics)
+      .filter(([ _, estimates]) => estimates.length > 0)
+      .map(([ key, estimates]) => [key, estimates.map(Number).reduce((a, b) => a + b, 0) / estimates.length])
+      .sort(([, a], [, b]) => a - b)
+      .forEach(([ key, average], i) => console.log(`[${i}] ${~~average} --- ${key}`));
+  });
+
+  beforeEach(async function () {
+    const messageHash = ethers.hexlify(ethers.randomBytes(32));
+    const privateKey = secp256r1.utils.randomPrivateKey();
+    const publicKey = [
+        secp256r1.getPublicKey(privateKey, false).slice(0x01, 0x21),
+        secp256r1.getPublicKey(privateKey, false).slice(0x21, 0x41),
+    ].map(ethers.hexlify)
+    const { r, s, recovery } = secp256r1.sign(messageHash.replace(/0x/, ''), privateKey);
+    const signature = [ r, s ].map(v => ethers.toBeHex(v, 32));
+
+    Object.assign(this, { messageHash, privateKey, publicKey, signature, recovery });
+  });
+
+  for (const { contract, signature, args, skip } of  [{
+    contract: '$Secp256r1_reference',
+    signature: '$Verify',
+    args: (publicKey, signature, digest) => [[...publicKey, "placeholder"], ...signature, digest],
+  },{
+    contract: '$Secp256r1_maxrobot',
+    signature: '$Verify',
+    args: (publicKey, signature, digest) => [...publicKey, signature, digest],
+  },{
+    contract: '$FCL_ecdsa',
+    signature: '$ecdsa_verify',
+    args: (publicKey, signature, digest) => [digest, ...signature, ...publicKey],
+  },{
+    contract: '$FCL_ecdsa_utils',
+    signature: '$ecdsa_verify',
+    args: (publicKey, signature, digest) => [digest, signature, ...publicKey],
+  },{
+    contract: '$P256',
+    signature: '$verify',
+    args: (publicKey, signature, digest) => [...publicKey, ...signature, digest],
+  }]) {
+    if (skip) continue;
+
+    const key = `${contract}.${signature}`;
+    describe(`${contract}.${signature}`, function () {
+      async function fixture() {
+        return { mock: await ethers.deployContract(contract) };
+      }
+
+      before(function () {
+        this.metrics[key] = [];
+      });
+
+      beforeEach(async function () {
+        Object.assign(this, await loadFixture(fixture));
+      });
+
+      Array(RUN_COUNT).fill().forEach((_, i, {length}) => {
+        it(`run ${i + 1}/${length}`, async function () {
+          expect(await this.mock.getFunction(signature).staticCall(...args(this.publicKey, this.signature, this.messageHash))).to.be.true;
+          this.metrics[key].push(await this.mock.getFunction(signature).estimateGas(...args(this.publicKey, this.signature, this.messageHash)));
+        });
+      });
+    });
+  }
+});
